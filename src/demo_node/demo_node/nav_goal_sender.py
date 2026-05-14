e85923d36cfd056627059992f9688efee3fe7604
unknown <domenico.quartodipalo@mail.polimi.com>
2026-05-14 19:50:38 +0200

    Modified nav_goal_sender to accept multiple goals

--- src/demo_node/demo_node/nav_goal_sender.py
+++ src/demo_node/demo_node/nav_goal_sender.py
@@ -4,6 +4,8 @@ from rclpy.action import ActionClient
 from nav2_msgs.action import NavigateToPose 
 from geometry_msgs.msg import PoseStamped
 from action_msgs.msg import GoalStatus
+import time
+
 
 class NavigationGoalSender(Node):
     def __init__(self):
@@ -21,7 +23,29 @@ class NavigationGoalSender(Node):
         self._action_client.wait_for_server()
         self.get_logger().info('Nav2 ready!')
 
-        self.send_goal(x=3.57, y=1.05, yaw=0.0)
+        self.goals = [
+            {'x': 3.57, 'y': 1.05, 'yaw': 0.0},
+            {'x': 2.0,  'y': -1.0,  'yaw': 0.0},
+            {'x': 1.0,  'y': 0.0,  'yaw': 0.0},
+        ]
+
+        self.current_goal_index = 0
+
+        self.send_next_goal()
+        self._last_feedback_time = 0.0
+    
+    def send_next_goal(self):
+        if self.current_goal_index < len(self.goals):
+            goal = self.goals[self.current_goal_index]
+            self.get_logger().info(
+                f"Goal {self.current_goal_index + 1}/{len(self.goals)} "
+                f"Going to -> x={goal['x']}, y={goal['y']}"
+            )
+            self.send_goal(**goal)
+        else:
+            self.get_logger().info("Goals completed, shutting down")
+
+        
 
     def send_goal(self, x, y, yaw):
         goal_msg = NavigateToPose.Goal()
@@ -39,7 +63,7 @@ class NavigationGoalSender(Node):
         goal_msg.pose.pose.orientation.z = 0.0
         goal_msg.pose.pose.orientation.w = 1.0
 
-        self.get_logger().info(f"Sending goal: x={x}, y = {y}")
+        # self.get_logger().info(f"Sending goal: x={x}, y = {y}")
 
         self._send_goal_future = self._action_client.send_goal_async(
             goal_msg,
@@ -58,13 +82,18 @@ class NavigationGoalSender(Node):
         self._get_result_future.add_done_callback(self.result_callback)
 
     def feedback_callback(self, feedback_msg):
-        pos = feedback_msg.feedback.current_pose.pose.position
-        self.get_logger().info(f'Position: x={pos.x:.2f}, y={pos.y:.2f}')
+        current_time = time.time()
+        if current_time - self._last_feedback_time >= 1.5:
+            pos = feedback_msg.feedback.current_pose.pose.position
+            self.get_logger().info(f'Position: x={pos.x:.2f}, y={pos.y:.2f}')
+            self._last_feedback_time = current_time
 
     def result_callback(self, future):
         result = future.result()
         if result.status == GoalStatus.STATUS_SUCCEEDED:
             self.get_logger().info('Goal REACHED! Robot stopped!')
+            self.current_goal_index += 1
+            self.send_next_goal()
         else:
             self.get_logger().error(f'Goal FAILED with Status: {result.status}')
 
